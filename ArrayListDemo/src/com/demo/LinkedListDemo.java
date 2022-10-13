064c273976dfa18901b22f4540ce75360697855c
sujit kudale <sujitkudale2020@gmail.com>
2022-10-13 22:16:31 +0530

    Added LinkedList Program

---
+++ ArrayListDemo/src/com/demo/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<String> list=new LinkedList<>();
+		list.add("Pawan");
+		list.add("Nayan");
+		list.add("Pramod");
+		
+		System.out.println(list);
+
+	}
+
+}
